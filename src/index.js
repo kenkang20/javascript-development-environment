897c494f94724d0377f9482d564d7419baa6fd16
ken <kkang@presagia.com>
2018-09-13 23:18:49 -0400

    Production Build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 //const courseValue = numeral(1000).format('$0,0.00');
 //console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
 
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 getUsers().then(result => {
     let usersBody = "";
@@ -20,4 +20,16 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes['data-id'].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 })
