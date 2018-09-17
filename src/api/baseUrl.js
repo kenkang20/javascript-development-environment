ccd23a1c50d0f328739fafb87a980d958eb8412c
ken <kkang@presagia.com>
2018-09-17 00:25:10 -0400

    Heroku integration

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,7 +2,7 @@ export default function getBaseUrl() {
     //const inDevelopment = window.location.hostname === 'localhost';
     //return inDevelopment ? 'http://localhost:3001/' : '/';
 
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://calm-reaches-21452.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
